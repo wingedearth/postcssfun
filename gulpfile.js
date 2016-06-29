ab2df36f3df5f44f6c6c95d5bfa8c09258edad89
Andrew Anissi <aanissi@legacy.com>
2016-06-29 14:40:43 -0700

    add packages to gulpfile and add stylus / postcss preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
-var gulp      = require('gulp');
-var postcss   = require('gulp-postcss');
-var csswring  = require('csswring');
-var lost      = require('lost');
+var gulp       = require('gulp');
+var postcss    = require('gulp-postcss');
+var csswring   = require('csswring');
+var lost       = require('lost');
+var stylus     = require('gulp-stylus')
+var processors = [csswring, lost];
 
 gulp.task('styles', function() {
-  var processors = [csswring, lost];
-
   return gulp.src('styles.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dest'));
 });
+
+gulp.task('fancystyles', function() {
+  return gulp.src('fancystyle.styl')
+    .pipe(stylus())
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dest'));
+})
 
 gulp.task('watch:styles', function() {
   gulp.watch('**/*.css', ['styles']);
